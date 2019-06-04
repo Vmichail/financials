2fe52c42a8029463b03636300e48b1ca763c4d46
V.Michail <43849032+Vmichail@users.noreply.github.com>
2019-06-04 20:04:05 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+
+
+
+var name='Vasileios Michail';
+$(document).ready(function(){
+    $(".name").html(name);
+    $("#Expenses").click(function(){
+        window.location.href = "expenses.html";
+    }
+    );
+  });
